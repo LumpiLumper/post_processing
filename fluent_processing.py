1d193a3eebaf0f3e43ad3bdf3037c74e8f5eebe2
LumpiLumper <jonholzer02@gmail.com>
2026-02-05 14:41:12 +0100

    new folder structure, progress works, excel works (momentum missing)

--- fluent_processing.py
+++ fluent_processing.py
@@ -1,11 +1,18 @@
 
 from __future__ import annotations
+import os
 import numpy as np
 import subprocess
 import sys
 import time
 from pathlib import Path
 from openpyxl import load_workbook
+import pandas as pd
+import shutil
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from ui import FluentProcessingUI
 
 
 rows: int = [11,    # Drag Frontwing
@@ -32,14 +39,31 @@ cols: int = [3,     # Drag Frontwing
 
 
 class FluentPostProcesser():
-    def __init__(self, fluent_exe_path: Path, case_folder_path: Path):
+    def __init__(self, fluent_exe_path: Path, case_folder_path: Path, ui: FluentProcessingUI):
         self.fluent_exe_path = fluent_exe_path
+        self.ui = ui
+        self.create_folder_struct(case_folder_path)
+        self.progress_flag = False
+
+    def create_folder_struct(self, case_folder_path):
         self.work_dir = Path(case_folder_path)
         self.case_file_path = list(case_folder_path.rglob("*.cas.h5"))[0]
-        self.out_dir = self.work_dir / "Processed"
+        self.out_dir = self.work_dir / "processed"
         self.out_dir.mkdir(parents=True, exist_ok=True)
-        self.jou_path = self.out_dir / "sequence_30_images.jou"
-    
+        self.jou_path = os.path.abspath(r"data\v0.1_sequence.jou")
+        self.images_dir = self.out_dir / "images"
+        self.images_dir.mkdir(parents=True, exist_ok=True)
+        self.front_vel_dir = self.images_dir / "front_vel"
+        self.front_vel_dir.mkdir(parents=True, exist_ok=True)
+        self.front_pr_dir = self.images_dir / "front_pressure"
+        self.front_pr_dir.mkdir(parents=True, exist_ok=True)
+        self.side_vel_dir = self.images_dir / "side_vel"
+        self.side_vel_dir.mkdir(parents=True, exist_ok=True)
+        self.side_pr_dir = self.images_dir / "side_pressure"
+        self.side_pr_dir.mkdir(parents=True, exist_ok=True)
+        self.forces_dir = self.out_dir / "forces"
+        self.forces_dir.mkdir(parents=True, exist_ok=True)
+
     def create_jou_content(self):
         # --- Parameters of sequence ---
         start_z = 0.0
@@ -52,22 +76,37 @@ class FluentPostProcesser():
         end_x = 1.850 
         x_positions = np.linspace(start_x, end_x, num_images_x)
         # --- creating jou content ---
+
+
         jou_content = f"""/file/read-case-data "{self.case_file_path.as_posix()}"
-            /file/confirm-overwrite? no
+            /file/set-batch-options no yes yes no
             /display/set-window 1
             /views/camera/projection orthographic
+            /views/apply-mirror-planes symmetry ()
+            
+            /report/forces/wall-forces yes 0 0 1 yes df.csv
+            /report/forces/wall-forces yes 1 0 0 yes drag.csv
+
+            /display/surface/iso-surface velocity velo_iso () () 5 ()
+            /display/set/contours surfaces velo_iso ()
+            /display/contour/pressure -600 100
+            /views/auto-scale   
+            /views/camera/target 0.5 0.1 0
+            /views/camera/position 0.5 0.1 -1
+            /views/camera/up-vector 0 1 0
+            /views/camera/zoom-camera 1.5
+            /display/save-picture "Processed/images/bottom_iso.png"
+
             /display/display/surface-mesh symmetry()
-            /views/auto-scale
             /views/camera/target 0.4 0 1
             /views/camera/position 0.4 1 1
-            /views/camera/up-vector 0 0 1
-            /views/camera/zoom-camera 9
-            """
+            /views/camera/up-vector 0 0 1   
+            """ 
 
         for i, z in enumerate(z_positions):
             s_name = f"plane_z_{i:02d}"
-            img_vel = f"Processed/Images/side_vel_{i:02d}.png"
-            img_pressure = f"Processed/Images/side_pressure_{i:02d}.png"
+            img_vel = f"Processed/Images/side_vel/side_vel_{i:02d}.png"
+            img_pressure = f"Processed/Images/side_pressure/side_pressure_{i:02d}.png"
             
             jou_content += f"""
                 ; --- Image {i+1}/{num_images_y} at Z = {z:.4f} ---
@@ -92,8 +131,8 @@ class FluentPostProcesser():
 
         for i, y in enumerate(x_positions):
             s_name = f"plane_x_{i:02d}"
-            img_vel = f"Processed/Images/front_vel_{i:02d}.png"
-            img_pressure = f"Processed/Images/front_pressure_{i:02d}.png"
+            img_vel = f"Processed/Images/front_vel/front_vel_{i:02d}.png"
+            img_pressure = f"Processed/Images/front_pressure/front_pressure_{i:02d}.png"
         
             
             jou_content += f"""
@@ -111,11 +150,16 @@ class FluentPostProcesser():
         
         self.jou_path.write_text(jou_content, encoding="utf-8")
 
-    def create_images(self, timeout_s = 2000):
+    def run_jou_file(self, timeout_s = 2000):
         print("Running Fluent (Mode Batch)...")
-        self.images_dir = self.out_dir / "Images"
-        self.images_dir.mkdir(parents=True, exist_ok=True)
-        cmd = [str(self.fluent_exe_path), "3d", "-t8", "-gu", "-i", str(self.jou_path)]
+
+        with open(self.jou_path) as f:
+            first_line = f.readline().strip()
+            # remove a possible leading ';' or '#' used as comment
+            first_line = first_line.lstrip(';#').strip()
+            n_lines = int(first_line)
+
+        cmd = [str(self.fluent_exe_path), "3d", "-t8", "-gu", "-i", self.jou_path]
         start = time.time()
         proc = subprocess.Popen(
             cmd,
@@ -128,9 +172,13 @@ class FluentPostProcesser():
             )
         assert proc.stdout is not None
         try:
-            for line in proc.stdout:
+            for line_counter, line in enumerate(proc.stdout):
+                if line_counter == 0:
+                    self.jou_progress(n_lines, line_counter)
+                    continue
                 print(line, end="")
                 sys.stdout.flush()
+                self.jou_progress(n_lines, line_counter)
                 if time.time() - start > timeout_s:
                     proc.kill()
                     raise TimeoutError(f"Process was longer than {timeout_s}s.")
@@ -145,11 +193,96 @@ class FluentPostProcesser():
         else:
             print(f"\n Error occoured in process: (Code {rc})")
 
+    def jou_progress(self, n_lines, line_counter):
+        if self.progress_flag == False:
+            self.progress_old = 0
+            self.progress = 0
+            self.progress_flag = True
+        else:
+            self.progress_old = self.progress
+        self.progress = round(line_counter/n_lines * 100)
+        if self.progress != self.progress_old:
+            self.ui.show_progress(self.progress)
+            
     def get_excel_data(self):
-        self.forces = [100, 200, 300, 600, 100, 200, 300, 600, 700]
+        source = self.work_dir / "df.csv"
+        destination = self.forces_dir
+        try:
+            shutil.copy2(source, destination)
+            os.remove(source)
+        except:
+            print("Couldn't find df.csv")
+        source = self.work_dir / "drag.csv"
+        try:
+            shutil.copy2(source, destination)
+            os.remove(source)
+        except:
+            print("Couldn't find drag.csv")
+
+        df = pd.read_csv(
+            self.forces_dir / "df.csv",
+            skiprows=19,
+            sep=r"\s+",
+            engine="python"
+            )
+
+        drag = pd.read_csv(
+            self.forces_dir / "drag.csv",     # chemin vers ton fichier
+            skiprows=19,        # ignore les 19 premières lignes
+            sep=r"\s+",         # 1 ou plusieurs espaces comme séparateur
+            engine="python"     # nécessaire pour les regex
+            )  
+
+        df_list = pd.to_numeric(df["Total"], errors="coerce").tolist()
+        drag_list = pd.to_numeric(drag["Total"], errors="coerce").tolist()
+
+        idx = {
+        "chassis": 1,
+        "front-wheel": 3,
+        "front-wing": 4,
+        "rear-wheel": 5,
+        "rear-wing": 6,
+        "sidepod": 8,
+        }
+
+        drag_fw = float(drag_list[idx["front-wing"]])
+        drag_sp = float(drag_list[idx["sidepod"]])
+        drag_rw = float(drag_list[idx["rear-wing"]])
+        drag_rwh = float(drag_list[idx["rear-wheel"]])
+        drag_fwh = float(drag_list[idx["front-wheel"]])
+        drag_ch = float(drag_list[idx["chassis"]])
+        drag_net = drag_fw + drag_sp + drag_rw + drag_rwh + drag_fwh + drag_ch
+
+        df_fw = float(df_list[idx["front-wing"]])
+        df_sp = float(df_list[idx["sidepod"]])
+        df_rw = float(df_list[idx["rear-wing"]])
+        df_rwh = float(df_list[idx["rear-wheel"]])
+        df_fwh = float(df_list[idx["front-wheel"]])
+        df_ch = float(df_list[idx["chassis"]])
+        df_net = df_fw + df_sp + df_rw + df_rwh + df_fwh + df_ch
+
+        moment_idx = None  # ex: 8
+        moment = float(df_list[moment_idx]) if moment_idx is not None else 0.0
+
+        self.forces = [
+        drag_fw,
+        drag_sp,
+        drag_rw,
+        drag_net,
+        df_fw,
+        df_sp,
+        df_rw,
+        df_net,
+        moment
+        ]
+        print(self.forces)
+        self.write_to_forcesheet()
 
     def write_to_forcesheet(self):
-        self.force_book = load_workbook("aero force sheet.xlsx")
+        source = r"data\aero force sheet.xlsx"
+        destination = self.forces_dir
+        shutil.copy2(source, destination)
+        self.force_book = load_workbook(self.forces_dir / "aero force sheet.xlsx")
         self.force_sheet = self.force_book.active
         '''
             forces must be in following format:
@@ -170,4 +303,5 @@ class FluentPostProcesser():
         for i, row in enumerate(rows):
             self.force_sheet.cell(row=rows[i], column=cols[i], value=self.forces[i])
         
-        self.force_book.save("aero force sheet.xlsx")
+        self.force_book.save(self.forces_dir / "aero force sheet.xlsx")
+        print(f"Forces saved in: {self.forces_dir}"+r"\aero force sheet.xlsx")
--- ui.py
+++ ui.py
@@ -2,6 +2,7 @@
 import tkinter as tk
 from tkinter import filedialog
 from pathlib import Path
+from PIL import Image, ImageTk
 
 from fluent_processing import FluentPostProcesser
 
@@ -9,33 +10,58 @@ class FluentProcessingUI():
     def __init__(self):
         self.fluent_exe_path = Path(r"C:\Program Files\ANSYS Inc\v252\fluent\ntbin\win64\fluent.exe")
         self.root = tk.Tk()
-        self.root.geometry("450x100")
+        self.root.geometry("800x400")
         self.root.title("Fluent Post-Processing")
+
+        try:
+            img_path = Path("data\logo_brt.png") # Chemin de ton image
+            img = Image.open(img_path)
+            img = img.resize((200, 200)) # Redimensionnement
+            self.photo = ImageTk.PhotoImage(img)
+            self.img_label = tk.Label(self.root, image=self.photo)
+            self.img_label.grid(row=2, column=0, rowspan=3, padx=20, pady=20)
+        except Exception as e:
+            print(f"Image not found: {e}")
+            self.img_label = tk.Label(self.root, text="[Image Missing]")
+            self.img_label.grid(row=1, column=0)
+
         # create buttons and lables
         self.bt_select_folder = tk.Button(self.root, text="Add new Case", command=self.select_folder)
         self.bt_view_images = tk.Button(self.root, text="View Images", command=self.view_images)
-        self.label = tk.Label(self.root, text="No Folder Selected")
+        self.lb_selected_folder = tk.Label(self.root, text="No Folder Selected")
         # place buttons and lables
         self.bt_view_images.grid(row=1, column=1)
         self.bt_select_folder.grid(row=1, column=2)
-        self.label.grid(row=2, column=2)
-        self.main_loop()
-
-    def main_loop(self):
+        self.lb_selected_folder.grid(row=2, column=2)
         self.root.mainloop()
 
     def select_folder(self):
         folder = filedialog.askdirectory()
         if folder:
-            self.label.config(text=f"Selected folder:\n{folder}")
+            self.lb_selected_folder.config(text=f"Selected folder:\n{folder}")
             self.case_file_path = Path(folder)
             self.bt_run_post_processing = tk.Button(self.root, text="Run Post-Processing", command=self.run_post_processing)
             self.bt_run_post_processing.grid(row=1, column=3)
 
     def view_images(self):
-        pass
+        folder = filedialog.askdirectory()
+        if folder:
+            self.root.quit()
+            self.image_slider = ImageSlider(folder)
 
     def run_post_processing(self):
-        fluent_processer = FluentPostProcesser(self.fluent_exe_path, self.case_file_path)
-        fluent_processer.create_jou_content()
-        fluent_processer.create_images()
+        fluent_processer = FluentPostProcesser(self.fluent_exe_path, self.case_file_path, self)
+        self.lb_progress = tk.Label(self.root, text="Initialising Fluent Ansys...")
+        self.lb_progress.grid(row=2, column=3)
+        self.root.update_idletasks()
+        fluent_processer.run_jou_file()
+        fluent_processer.get_excel_data()
+        self.lb_progress.config(text=f"done!")
+    
+    def show_progress(self, progress):
+        self.lb_progress.config(text=f"Progress: {progress}%")
+        self.root.update_idletasks()
+
+class ImageSlider():
+    def __init__(self, folder: Path):
+        self.folder = folder
